1090a224d9e2550f8a7aac848f9c5850604b2224
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2026-02-14 21:44:56 +0900

    Update Next.js configuration to remove custom headers and set output to 'export'; add dynamic export option in manifest.

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-        ],
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
