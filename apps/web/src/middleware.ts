573a36c07d628a6d530339ee247649d6489b9a10
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2026-03-03 14:32:28 +0900

    build修正

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|sitemap\\.xml|robots\\.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
